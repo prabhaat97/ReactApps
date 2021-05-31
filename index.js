63bfbff5c403e06a51262defc796d373b1ad378a
Prabhat <prabhat.kumar@successive.tech>
2021-05-31 12:09:14 +0530

    check

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+
+export default class TectFieldComponent extends React.Component{
+    render(){
+        return( 
+            <view>
+                <Text>
+                    First Component
+                </Text>
+            </view>
+        )
+    }
+}
