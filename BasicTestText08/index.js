0c8bf60500ed0c6eaf914f5e77e4bb3b5b1d54c4
Prabhat <prabhat.kumar@successive.tech>
2021-06-01 01:13:49 +0530

    React pack

---
+++ BasicTestText08/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View,TextInput} from 'react-native';
+
+
+export default class BasicTestText extends React.Component{
+    render(){
+        return( 
+            <view>
+                <TextInput>
+                    
+                </TextInput>
+            </view>
+        )
+    }
+}
